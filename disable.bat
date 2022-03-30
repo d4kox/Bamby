8fddf18a6214973e2a12fb88c89e490b59d61516
d4kox <101132138+d4kox@users.noreply.github.com>
2022-03-30 13:50:01 -0300

    Delete disable.bat

--- disable.bat
+++
@@ -1,7 +0,0 @@
-C:
-cd "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu"
-rd Bambi
-cd "%UserProfile%\Documents"
-rd Bamby
-echo "[*] Bambi se ha desinstalado correctamente"
-timeout /nobreak 5
